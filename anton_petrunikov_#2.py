420fb3003253e5c11db5a3b766ac78c1a72b14bd
AntiPetrun <104591875+AntiPetrun@users.noreply.github.com>
2022-05-31 01:18:42 +0300

    add homework #2

--- anton_petrunikov_#2.py
+++ anton_petrunikov_#2.py
@@ -1,1 +1,6 @@
-
+from decimal import Decimal
+weight = Decimal(input('Enter your weight in kg: '))
+height = Decimal(input('Enter your height in m: '))
+bmi_index = round(Decimal(weight) / Decimal(height ** 2))
+bmi_scale = '20' + '=' * (bmi_index - 21) + '|' + '=' * (49 - bmi_index) + '50'
+print(f'Your body mass inde: {bmi_index}\n' + bmi_scale)
